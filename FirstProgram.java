dcb7d47c24a5952e26deaf5ae998ae0581841d62
Kumkum-001 <nayakkumkum003@gmail.com>
2026-02-14 16:36:35 +0530

    First program

---
+++ FirstProgram.java
@@ -0,0 +1,15 @@
+
+
+public class FirstProgram {
+
+	public static void main(String[] args) 
+	{
+	
+		System.out.println("Hello Everyone");
+		
+		
+	}
+	
+	
+	
+}
--- PrimitiveDatatypesDemo.java
+++ PrimitiveDatatypesDemo.java
@@ -1,4 +1,4 @@
-package com.tnsif.dayone;
+
 
 public class PrimitiveDatatypesDemo {
 
